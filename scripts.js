8aaeb99842ba3ecf016dd390ea6db4cce2e8885b
Maciej Rut <wirtmaciej@gmail.com>
2017-08-11 21:19:21 +0200

    index scripts modified

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 $(function(){
 	var span = $("span");
-	span.each(function(index,element) {
-		if(index % 2 == 0) {
+	span.each(function(index, element) {
+		if (index % 2 === 0) {
 			$(element).css('color', 'red');
 		};
 	});
